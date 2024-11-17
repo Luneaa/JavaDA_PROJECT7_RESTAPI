f8557a7f50c29e325d1d89eba1bf568143f66173
Luneaa <sanchez.flora11@gmail.com>
2024-11-17 12:34:24 +0100

    Fix build issues

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,15 +1,85 @@
 package com.nnk.springboot.domain;
 
-import org.hibernate.validator.constraints.Length;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
 
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
+    @Id
+    private Integer id;
+
+    private Integer curveId;
+
+    private Timestamp asOfDate;
+
+    private Double term;
+
+    private Double value;
+
+    public CurvePoint(Integer curveId, Double term, Double value) {
+        this.curveId = curveId;
+        this.term = term;
+        this.value = value;
+    }
+
+    public CurvePoint() {
+        
+    }
+
+    public Integer getCurveId() {
+        return curveId;
+    }
+
+    public void setCurveId(Integer curveId) {
+        this.curveId = curveId;
+    }
+
+    public Timestamp getAsOfDate() {
+        return asOfDate;
+    }
+
+    public void setAsOfDate(Timestamp asOfDate) {
+        this.asOfDate = asOfDate;
+    }
+
+    public Double getTerm() {
+        return term;
+    }
+
+    public void setTerm(Double term) {
+        this.term = term;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    private Timestamp creationDate;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
     // TODO: Map columns in data table CURVEPOINT with corresponding java fields
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,72 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "rating")
 public class Rating {
+    @Id
+    private Integer id;
+
+    private String moodysRating;
+
+    private String sandPRating;
+
+    private String fitchRating;
+
+    private Integer orderNumber;
+
+    public Rating(String moodysRating, String sandPRating, String fitchRating, Integer orderNumber) {
+        this.moodysRating = moodysRating;
+        this.sandPRating = sandPRating;
+        this.fitchRating = fitchRating;
+        this.orderNumber = orderNumber;
+    }
+
+    public Rating() {
+
+    }
+
+    public String getMoodysRating() {
+        return moodysRating;
+    }
+
+    public void setMoodysRating(String moodysRating) {
+        this.moodysRating = moodysRating;
+    }
+
+    public String getSandPRating() {
+        return sandPRating;
+    }
+
+    public void setSandPRating(String sandPRating) {
+        this.sandPRating = sandPRating;
+    }
+
+    public String getFitchRating() {
+        return fitchRating;
+    }
+
+    public void setFitchRating(String fitchRating) {
+        this.fitchRating = fitchRating;
+    }
+
+    public Integer getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(Integer orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
     // TODO: Map columns in data table RATING with corresponding java fields
 }
