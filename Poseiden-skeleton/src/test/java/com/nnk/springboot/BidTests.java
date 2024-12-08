342d3f5fa9316a8038f6c95630bd6cb66d2c106c
Luneaa <sanchez.flora11@gmail.com>
2024-12-08 12:48:44 +0100

    Clean existing unit test

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/BidTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/BidTests.java
@@ -26,16 +26,16 @@ public class BidTests {
 		// Save
 		bid = bidListRepository.save(bid);
 		Assert.assertNotNull(bid.getBidListId());
-		Assert.assertEquals(bid.getBidQuantity(), 10d, 10d);
+		Assert.assertEquals((Double) 10d, bid.getBidQuantity());
 
 		// Update
 		bid.setBidQuantity(20d);
 		bid = bidListRepository.save(bid);
-		Assert.assertEquals(bid.getBidQuantity(), 20d, 20d);
+		Assert.assertEquals((Double) 20d, bid.getBidQuantity());
 
 		// Find
 		List<BidList> listResult = bidListRepository.findAll();
-		Assert.assertTrue(listResult.size() > 0);
+        Assert.assertFalse(listResult.isEmpty());
 
 		// Delete
 		Integer id = bid.getBidListId();
